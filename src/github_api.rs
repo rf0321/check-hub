03e5fd4b8f37b63a14de754df995c4b931d11d1d
Evil Rabbit <jkekun@gmail.com>
2018-03-10 16:44:41 +0900

    Update github_api.rs

--- src/github_api.rs
+++ src/github_api.rs
@@ -9,14 +9,6 @@ impl GithubAPI{
     pub fn new() -> GithubAPI{
         GithubAPI {}
     }
-    fn url_with_param(&self,param:&str)-> &str{
-        let username = GITHUB_USER_NAME;
-        let token = API_TOKEN;
-        let url = format!(
-            "{}{}{}{}{}{}",API_BASE_URL,"users/",username,param,"?accesstoken=",token
-        );
-        return self.convert_to_statictype(url);
-    }
     pub fn profile(&self)-> &str{
         let username = GITHUB_USER_NAME;
         let token = API_TOKEN; 
